650213776c1188961042532cfac795bb39e0abc1
khylle02 <canamaque02@gmail.com>
2024-02-19 07:11:10 +0800

    added a rating component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,27 @@
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import React from 'react'
+import Rating from './Rating';
 
 function Product({product}) {
   return (
     <Card className='my-3 p-3 rounded' >
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
             <Card.Img src={product.image} variant='top'/>
-        </a>
+        </Link>
+
         <Card.Body>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Title as="p">
                     <strong>{product.name}</strong>
                 </Card.Title>
-            </a>
+            </Link>
+
+            <Card.Text as='div'>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+            </Card.Text>
+
             <Card.Text as="h3">
                 ₱{product.price}
             </Card.Text>
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,38 @@
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 
-function HomeScreen({products}) {
+
+const products = [
+  {
+    _id: '1',
+    name: 'ALL TIME LOW Don’t Panic: It’s Longer Now Orange Vinyl',
+    image: '/images/ALL-TIME-LOW-Dont-Panic-Its-Longer-Now-Orange-Vinyl.webp',
+    description:
+      'This is a legit, original, officially licensed merchandise All Time Low purchased directly from the band’s record label, online store, or licensed distributor.',
+    brand: 'Hopeless Records',
+    category: 'Pop-Punk',
+    price: 595.00,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'ALL TIME LOW Last Young Renegade (Young Renegades Version) Teal Vinyl',
+    image: '/images/ALL-TIME-LOW-Last-Young-Renegade-Alternate-Cover-Teal-Vinyl.webp',
+    description:
+      'This is a legit, original, officially licensed merchandise All Time Low purchased directly from the band’s record label, online store, or licensed distributor.',
+    brand: 'Hopeless Records',
+    category: 'Pop-Punk',
+    price: 895.00,
+    countInStock: 1,
+    rating: 5,
+    numReviews: 12,
+  }
+];
+
+
+function HomeScreen() {
   return (
     <>
         <h1>Latest Products</h1>
