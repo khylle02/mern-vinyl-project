0ac789ce596637a1d62076edbd20b9db731d2882
khylle02 <canamaque02@gmail.com>
2024-02-19 05:41:27 +0800

    header & footer created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
+import {Container } from "react-bootstrap";
+import Header from "./components/Header";
+import React  from 'react';
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <h1>Welcome to Vinyl Shop</h1>
-}
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+      <h1>Welcome to Vinyl Shop</h1>
+      </main>
+      <Footer />
+    </>
+  )
+};
 
 export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import './index.css';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
